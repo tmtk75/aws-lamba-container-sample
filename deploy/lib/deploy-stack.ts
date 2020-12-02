2a162a3a5234cc987b005f84444a6d00bf0d7ac4
Tomotaka Sakuma <ktmtmks@gmail.com>
2020-12-02 09:45:55 +0900

    add: an ECR repo

--- deploy/lib/deploy-stack.ts
+++ deploy/lib/deploy-stack.ts
@@ -1,7 +1,8 @@
 import * as cdk from "@aws-cdk/core";
-import { Function, AssetCode, Runtime } from "@aws-cdk/aws-lambda";
+import { DockerImageFunction, DockerImageCode } from "@aws-cdk/aws-lambda";
 import { Role, ServicePrincipal, ManagedPolicy } from "@aws-cdk/aws-iam";
 import { Vpc, Subnet, SecurityGroup } from "@aws-cdk/aws-ec2";
+import { Repository } from "@aws-cdk/aws-ecr";
 
 export class DeployStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -24,7 +25,6 @@ export class DeployStack extends cdk.Stack {
       throw new Error(`You have to set SUBNET_ID.`);
     }
 
-
     const executionLambdaRole = new Role(this, "code-runner-role", {
       roleName: `${prefix}lambdaSecureExecutionRole`,
       assumedBy: new ServicePrincipal("lambda.amazonaws.com"),
@@ -36,12 +36,13 @@ export class DeployStack extends cdk.Stack {
     });
 
     const vpc = Vpc.fromLookup(this, "anVpc", { vpcId });
+    const repo = new Repository(this, "aRepo", {
+      repositoryName: `${prefix}sample-repo`,
+    });
 
-    new Function(this, "code-runner", {
-      functionName: `${prefix}-simple-func`,
-      runtime: Runtime.NODEJS_12_X,
-      code: AssetCode.fromAsset("../lib"),
-      handler: "lambda/index.handler",
+    new DockerImageFunction(this, "code-runner", {
+      // functionName: `${prefix}-simple-func`,
+      code: DockerImageCode.fromEcr(repo),
       timeout: cdk.Duration.seconds(5),
       role: executionLambdaRole,
 
@@ -49,9 +50,7 @@ export class DeployStack extends cdk.Stack {
       //   TZ: "Asia/Tokyo",
       // },
       vpc,
-      securityGroups: [
-        SecurityGroup.fromSecurityGroupId(this, "aSG", sgId),
-      ],
+      securityGroups: [SecurityGroup.fromSecurityGroupId(this, "aSG", sgId)],
       // subnet-37a6a143, subnet-66c29820
       vpcSubnets: {
         // subnetType: ec2.SubnetType.PRIVATE,
