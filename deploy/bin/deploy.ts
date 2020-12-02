ef111d93240f5d313c42f84b41cb36808d8c7e54
Tomotaka Sakuma <ktmtmks@gmail.com>
2020-12-02 09:40:35 +0900

    add: configure envvars.

--- deploy/bin/deploy.ts
+++ deploy/bin/deploy.ts
@@ -1,7 +1,13 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { DeployStack } from '../lib/deploy-stack';
+import "source-map-support/register";
+import * as cdk from "@aws-cdk/core";
+import { DeployStack } from "../lib/deploy-stack";
 
 const app = new cdk.App();
-new DeployStack(app, 'AWSLambdaWithContainerSample');
+new DeployStack(app, "AWSLambdaWithContainerSample", {
+  // https://docs.aws.amazon.com/cdk/latest/guide/environments.html
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
+});
