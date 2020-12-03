8b2cc96cf0e68f1b26d799f5f14268a81d9a8e4b
Tomotaka Sakuma <ktmtmks@gmail.com>
2020-12-03 23:52:36 +0900

    fix: move ecr out to another stack.

--- deploy/bin/deploy.ts
+++ deploy/bin/deploy.ts
@@ -2,12 +2,16 @@
 import "source-map-support/register";
 import * as cdk from "@aws-cdk/core";
 import { DeployStack } from "../lib/deploy-stack";
+import { EcrStack } from "../lib/ecr-stack";
 
 const app = new cdk.App();
-new DeployStack(app, "AWSLambdaWithContainerSample", {
+const opts = {
   // https://docs.aws.amazon.com/cdk/latest/guide/environments.html
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: process.env.CDK_DEFAULT_REGION,
   },
-});
+};
+
+new EcrStack(app, "AWSLambdaWithContainerSampleRepository", opts);
+new DeployStack(app, "AWSLambdaWithContainerSample", opts);
--- deploy/lib/deploy-stack.ts
+++ deploy/lib/deploy-stack.ts
@@ -3,7 +3,6 @@ import { DockerImageFunction, DockerImageCode } from "@aws-cdk/aws-lambda";
 import { Role, ServicePrincipal, ManagedPolicy } from "@aws-cdk/aws-iam";
 import { Vpc, Subnet, SecurityGroup } from "@aws-cdk/aws-ec2";
 import { Repository } from "@aws-cdk/aws-ecr";
-import { RemovalPolicy } from "@aws-cdk/core";
 
 export class DeployStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -27,7 +26,6 @@ export class DeployStack extends cdk.Stack {
     }
 
     const roleName = `${prefix}lambdaSecureExecutionRole`;
-    // console.log(`roleName:`, roleName);
     const executionLambdaRole = new Role(this, "code-runner-role", {
       roleName,
       assumedBy: new ServicePrincipal("lambda.amazonaws.com"),
@@ -39,11 +37,11 @@ export class DeployStack extends cdk.Stack {
     });
 
     const vpc = Vpc.fromLookup(this, "anVpc", { vpcId });
-    const repo = new Repository(this, "aRepo", {
-      repositoryName: `${prefix}sample-repo`,
-      removalPolicy: RemovalPolicy.DESTROY,
-    });
-
+    const repo = Repository.fromRepositoryName(
+      this,
+      "aRepo",
+      `${prefix}sample-repo`
+    );
     new DockerImageFunction(this, "code-runner", {
       functionName: `${prefix}simple-func`,
       code: DockerImageCode.fromEcr(repo),
